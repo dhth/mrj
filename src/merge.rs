0749df3b26d6500478118f15a62a0a0b96df4266
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2025-05-23 04:01:10 +0200

    minor visual tweaks

--- src/merge.rs
+++ src/merge.rs
@@ -28,7 +28,7 @@ pub async fn merge_pr(client: Octocrab, config: Config, dry_run: bool) -> anyhow
 
         if page.items.is_empty() {
             println!();
-            print_warning("no PRs");
+            print_absence("no PRs");
             continue;
         }
 
@@ -41,20 +41,20 @@ pub async fn merge_pr(client: Octocrab, config: Config, dry_run: bool) -> anyhow
 
             match &pull_request.user {
                 Some(trusted_user) if config.trusted_authors.contains(&trusted_user.login) => {
-                    print_info(&format!(
+                    print_qualification(&format!(
                         "{} \"{}\" is in the list of trusted users",
                         AUTHOR, trusted_user.login
                     ));
                 }
                 Some(other_user) => {
-                    print_warning(&format!(
+                    print_disqualification(&format!(
                         "{} \"{}\" is not in the list of trusted users",
                         AUTHOR, other_user.login
                     ));
                     continue;
                 }
                 None => {
-                    print_warning(&format!(
+                    print_disqualification(&format!(
                         "{} Github sent an empty user; skipping as I can't make any assumptions here",
                         AUTHOR
                     ));
@@ -81,13 +81,13 @@ pub async fn merge_pr(client: Octocrab, config: Config, dry_run: bool) -> anyhow
             for check in &checks.check_runs {
                 match check.conclusion.as_deref() {
                     Some("success") => {
-                        print_info(&format!("{} \"{}\": success", CHECK, check.name));
+                        print_qualification(&format!("{} \"{}\": success", CHECK, check.name));
                     }
                     Some("skipped") => {
                         if config.merge_if_checks_skipped.unwrap_or(true) {
-                            print_info(&format!("{} \"{}\": skipped", CHECK, check.name));
+                            print_qualification(&format!("{} \"{}\": skipped", CHECK, check.name));
                         } else {
-                            print_warning(&format!(
+                            print_disqualification(&format!(
                                 "{} \"{}\" skipped; merge_if_checks_skipped is false, so skipping",
                                 CHECK, check.name
                             ));
@@ -96,15 +96,15 @@ pub async fn merge_pr(client: Octocrab, config: Config, dry_run: bool) -> anyhow
                         }
                     }
                     Some(non_successful_conclusion) => {
-                        print_warning(&format!(
-                            "{} \"{}\" {} ❌; skipping",
+                        print_disqualification(&format!(
+                            "{} \"{}\" {}; skipping",
                             CHECK, check.name, non_successful_conclusion
                         ));
                         skip = true;
                         break;
                     }
                     None => {
-                        print_warning(&format!(
+                        print_disqualification(&format!(
                             "{} Github returned with an empty conclusion for the check {}; skipping as I can't make any assumptions here",
                             CHECK, check.name,
                         ));
@@ -119,32 +119,32 @@ pub async fn merge_pr(client: Octocrab, config: Config, dry_run: bool) -> anyhow
             }
 
             if !&checks.check_runs.is_empty() {
-                print_info("checks look good");
+                print_qualification("checks look good");
             }
 
             match pr.mergeable_state.as_ref() {
                 Some(state) => match state {
                     MergeableState::Clean => {
-                        print_info(&format!("{} \"clean\"", STATE));
+                        print_qualification(&format!("{} \"clean\"", STATE));
                     }
                     MergeableState::Blocked => {
                         if config.merge_if_blocked.unwrap_or(false) {
-                            print_info(&format!(
+                            print_qualification(&format!(
                                 "{} \"blocked\" (merge_if_blocked is true)",
                                 STATE
                             ));
                         } else {
-                            print_warning(&format!("{} blocked ❌; skipping", STATE));
+                            print_disqualification(&format!("{} blocked; skipping", STATE));
                             continue;
                         }
                     }
                     other => {
-                        print_warning(&format!("{} {:?} ❌; skipping", STATE, other));
+                        print_disqualification(&format!("{} {:?}; skipping", STATE, other));
                         continue;
                     }
                 },
                 None => {
-                    print_warning(&format!(
+                    print_disqualification(&format!(
                         "{} Github returned with an empty mergeable state; skipping as I can't make any assumptions here",
                         STATE
                     ));
@@ -153,7 +153,7 @@ pub async fn merge_pr(client: Octocrab, config: Config, dry_run: bool) -> anyhow
             }
 
             if !dry_run {
-                print_info("PR matches all criteria, merging...");
+                print_qualification("PR matches all criteria, merging...");
                 client
                     .pulls(&repo.owner, &repo.repo)
                     .merge(pr.number)
@@ -165,7 +165,7 @@ pub async fn merge_pr(client: Octocrab, config: Config, dry_run: bool) -> anyhow
 
                 break;
             } else {
-                print_info("PR matches all criteria, would've been merged ✅");
+                print_qualification("PR matches all criteria, would've been merged ✅");
             }
         }
     }
@@ -199,11 +199,15 @@ fn print_pr_info(msg: &str) {
     println!("\n{}", msg.purple());
 }
 
-fn print_info(msg: &str) {
+fn print_qualification(msg: &str) {
     println!("\t{}", msg.blue());
 }
 
-fn print_warning(msg: &str) {
+fn print_disqualification(msg: &str) {
+    println!("\t{} ❌", msg.yellow());
+}
+
+fn print_absence(msg: &str) {
     println!("\t{}", msg.yellow());
 }
 
