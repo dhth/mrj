5ca49584266f6cd3745f03aa27204964fe1eab12
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2025-05-26 22:17:46 +0200

    refactor code structure (#5)

---
+++ src/merge/log.rs
@@ -0,0 +1,228 @@
+use crate::domain::{Disqualification, Qualification, RunStats};
+use anyhow::Context;
+use colored::Colorize;
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::path::Path;
+
+const BANNER: &str = include_str!("assets/banner.txt");
+const AUTHOR: &str = "[ author ]  ";
+const HEAD: &str = "[ head   ]  ";
+const CHECK: &str = "[ check  ]  ";
+const STATE: &str = "[ state  ]  ";
+
+pub(super) struct RunLog {
+    write_to_file: bool,
+    lines: Vec<String>,
+    stats: RunStats,
+}
+
+impl RunLog {
+    pub(super) fn new(output: bool) -> Self {
+        RunLog {
+            write_to_file: output,
+            lines: vec![],
+            stats: RunStats::default(),
+        }
+    }
+
+    pub fn banner(&mut self, dry_run: bool) {
+        println!("{}", BANNER.green().bold());
+        if dry_run {
+            println!("{}", "                         dry run".yellow());
+        }
+        println!("\n");
+
+        if self.write_to_file {
+            self.lines.push(BANNER.to_string());
+            if dry_run {
+                self.lines
+                    .push("                         dry run".to_string());
+            }
+            self.lines.push("\n".to_string());
+        }
+    }
+
+    pub fn info(&mut self, message: &str) {
+        println!("[INFO] {}", message);
+
+        if self.write_to_file {
+            self.lines.push(format!("[INFO] {}", message));
+        }
+    }
+
+    pub fn repo_info(&mut self, name: &str) {
+        println!(
+            "{}",
+            format!(
+                r#"
+
+=============
+  {}
+============="#,
+                name
+            )
+            .cyan()
+        );
+
+        if self.write_to_file {
+            self.lines.push(format!(
+                r#"
+
+=============
+  {}
+============="#,
+                name
+            ));
+        }
+    }
+
+    pub fn pr_info(&mut self, msg: &str) {
+        println!("{}", msg.purple());
+
+        if self.write_to_file {
+            self.lines.push(msg.to_string());
+        }
+    }
+
+    pub fn qualification(&mut self, q: Qualification) {
+        let msg = match q {
+            Qualification::Head(h) => {
+                format!("{} \"{}\" matches the allowed head pattern", HEAD, h)
+            }
+            Qualification::User(a) => {
+                format!("{} \"{}\" is in the list of trusted authors", AUTHOR, a)
+            }
+            Qualification::Check { name, conclusion } => {
+                format!(
+                    "{} \"{}\" concluded with desired status: \"{}\"",
+                    CHECK, name, conclusion,
+                )
+            }
+            Qualification::State(s) => format!("{} \"{}\" looks good", STATE, s),
+        };
+
+        println!("        {}", msg.blue());
+
+        if self.write_to_file {
+            self.lines.push(format!("        {}", msg));
+        }
+    }
+
+    pub fn disqualification(&mut self, dq: Disqualification) {
+        let msg = match dq {
+            Disqualification::Head(h) => {
+                format!("{} \"{}\" doesn't match the allowed head pattern", HEAD, h)
+            }
+            Disqualification::User(maybe_author) => match maybe_author {
+                Some(a) => format!("{} \"{}\" is not in the list of trusted authors", AUTHOR, a),
+                None => format!(
+                    "{} Github sent an empty user; skipping as I can't make any assumptions here",
+                    AUTHOR
+                ),
+            },
+            Disqualification::Check { name, conclusion } => match conclusion {
+                Some(c) => format!(
+                    "{} \"{}\" concluded with undesired status: \"{}\"",
+                    CHECK, name, c
+                ),
+                None => format!(
+                    "{} Github returned with an empty conclusion for the check {}; skipping as I can't make any assumptions here",
+                    CHECK, name,
+                ),
+            },
+            Disqualification::State(maybe_state) => match maybe_state {
+                Some(s) => format!("{} \"{}\" is undesirable", STATE, s),
+                None => format!(
+                    "{} Github returned with an empty mergeable state; skipping as I can't make any assumptions here",
+                    STATE
+                ),
+            },
+        };
+
+        println!("        {} ❌", msg.yellow());
+
+        if self.write_to_file {
+            self.lines.push(format!("        {} ❌", msg));
+        }
+
+        self.stats.record_disqualification();
+    }
+
+    pub fn absence(&mut self, msg: &str) {
+        println!("        {}", msg.yellow());
+
+        if self.write_to_file {
+            self.lines.push(format!("        {}", msg));
+        }
+    }
+
+    pub fn merge(&mut self, msg: &str, dry_run: bool) {
+        println!("        {}", msg.green());
+
+        if self.write_to_file {
+            self.lines.push(format!("        {}", msg));
+        }
+
+        if !dry_run {
+            self.stats.record_merge();
+        }
+    }
+
+    pub fn empty_line(&mut self) {
+        println!();
+
+        if self.write_to_file {
+            self.lines.push("".to_string());
+        }
+    }
+
+    pub fn error(&mut self, error: anyhow::Error) {
+        println!("{}", format!("        error 😵: {}", error).red());
+
+        if self.write_to_file {
+            self.lines.push(format!("        error 😵: {}", error));
+        }
+
+        self.stats.record_error();
+    }
+
+    pub fn write_output<P>(&mut self, write_to_file: bool, output_file: P) -> anyhow::Result<()>
+    where
+        P: AsRef<Path>,
+    {
+        let stats = format!(
+            r#"
+===========================
+
+  Stats
+
+  #PRs merged         : {}
+  #PRs disqualified   : {}
+  #errors encountered : {}
+
+==========================="#,
+            self.stats.num_merges, self.stats.num_disqualifications, self.stats.num_errors
+        );
+
+        println!("{}", &stats.green());
+
+        if !write_to_file {
+            return Ok(());
+        }
+
+        self.lines.push(stats);
+
+        let mut file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(output_file)
+            .context("couldn't open a handle to the output file")?;
+
+        file.write_all(self.lines.join("\n").as_bytes())
+            .context("couldn't write output to file")?;
+
+        Ok(())
+    }
+}
---
+++ src/merge/mod.rs
@@ -0,0 +1,251 @@
+mod log;
+
+use crate::config::Config;
+use crate::domain::{Disqualification as DQ, Qualification as Q, Repo};
+use anyhow::Context;
+use chrono::Utc;
+use log::RunLog;
+use octocrab::Octocrab;
+use octocrab::{
+    models::pulls::MergeableState,
+    params::{State, repos::Commitish},
+};
+use std::path::Path;
+
+pub async fn merge_prs<P>(
+    client: Octocrab,
+    config: Config,
+    repos_override: Vec<Repo>,
+    output: bool,
+    output_file: P,
+    dry_run: bool,
+) -> anyhow::Result<()>
+where
+    P: AsRef<Path>,
+{
+    let mut l = RunLog::new(output);
+
+    let repos_to_use = if repos_override.is_empty() {
+        &config.repos
+    } else {
+        &repos_override
+    };
+
+    l.banner(dry_run);
+
+    let start = Utc::now();
+    l.info(&format!("The time right now is {}", start));
+
+    if let Some(b) = &config.base_branch {
+        l.info(&format!(
+            "I'm only looking for PRs where the base branch is \"{}\"",
+            b
+        ));
+    }
+
+    if config.merge_if_blocked {
+        l.info("I will merge PRs even if they're blocked");
+    }
+
+    if !config.merge_if_checks_skipped {
+        l.info("I won't merge PRs if checks are skipped");
+    }
+
+    for repo in repos_to_use {
+        l.repo_info(&repo.repo);
+
+        let pulls = client.pulls(&repo.owner, &repo.repo);
+
+        let mut page_builder = pulls.list().state(State::Open).per_page(100);
+
+        if let Some(base_branch) = &config.base_branch {
+            page_builder = page_builder.base(base_branch);
+        }
+
+        let page = match page_builder.send().await.context("couldn't get PRs") {
+            Ok(p) => p,
+            Err(err) => {
+                l.error(err);
+                continue;
+            }
+        };
+
+        if page.items.is_empty() {
+            l.empty_line();
+            l.absence("no PRs");
+            continue;
+        }
+
+        for pull_request in &page {
+            l.pr_info(&format!(
+                r#"
+-> checking PR #{}
+        {}
+        {}"#,
+                pull_request.number,
+                pull_request.title.clone().unwrap_or_default(),
+                pull_request
+                    .html_url
+                    .as_ref()
+                    .map(|url| url.to_string())
+                    .unwrap_or_default(),
+            ));
+
+            if let Some(head_pattern) = &config.head_pattern {
+                let head_ref = pull_request.head.ref_field.clone();
+                if head_pattern.re.is_match(&head_ref) {
+                    l.qualification(Q::Head(head_ref));
+                } else {
+                    l.disqualification(DQ::Head(head_ref));
+                    continue;
+                }
+            }
+
+            match &pull_request.user {
+                Some(trusted_user) if config.trusted_authors.contains(&trusted_user.login) => {
+                    l.qualification(Q::User(trusted_user.login.clone()));
+                }
+                Some(other_user) => {
+                    l.disqualification(DQ::User(Some(other_user.login.clone())));
+                    continue;
+                }
+                None => {
+                    l.disqualification(DQ::User(None));
+                    continue;
+                }
+            }
+
+            let pr = match client
+                .pulls(&repo.owner, &repo.repo)
+                .get(pull_request.number)
+                .await
+                .context("couldn't get details")
+            {
+                Ok(pr) => pr,
+                Err(err) => {
+                    l.error(err);
+                    continue;
+                }
+            };
+
+            let pr_head_ref = pr.head.sha;
+
+            let checks = match client
+                .checks(&repo.owner, &repo.repo)
+                .list_check_runs_for_git_ref(Commitish::from(pr_head_ref.clone()))
+                .send()
+                .await
+                .context("couldn't get pr checks")
+            {
+                Ok(c) => c,
+                Err(err) => {
+                    l.error(err);
+                    continue;
+                }
+            };
+
+            let mut skip = false;
+            for check in &checks.check_runs {
+                match check.conclusion.as_deref() {
+                    Some("success") => {
+                        l.qualification(Q::Check {
+                            name: check.name.clone(),
+                            conclusion: "success".to_string(),
+                        });
+                    }
+                    Some("skipped") => {
+                        if config.merge_if_checks_skipped {
+                            l.qualification(Q::Check {
+                                name: check.name.clone(),
+                                conclusion: "success".to_string(),
+                            });
+                        } else {
+                            l.disqualification(DQ::Check {
+                                name: check.name.clone(),
+                                conclusion: Some("skipped".to_string()),
+                            });
+                            skip = true;
+                            break;
+                        }
+                    }
+                    Some(non_successful_conclusion) => {
+                        l.disqualification(DQ::Check {
+                            name: check.name.clone(),
+                            conclusion: Some(non_successful_conclusion.to_string()),
+                        });
+                        skip = true;
+                        break;
+                    }
+                    None => {
+                        l.disqualification(DQ::Check {
+                            name: check.name.clone(),
+                            conclusion: None,
+                        });
+                        skip = true;
+                        break;
+                    }
+                }
+            }
+
+            if skip {
+                continue;
+            }
+
+            match pr.mergeable_state.as_ref() {
+                Some(state) => match state {
+                    MergeableState::Clean => {
+                        l.qualification(Q::State("clean".to_string()));
+                    }
+                    MergeableState::Blocked if config.merge_if_blocked => {
+                        l.qualification(Q::State("blocked".to_string()));
+                    }
+                    other => {
+                        l.disqualification(DQ::State(Some(format!("{:?}", other))));
+                        continue;
+                    }
+                },
+                None => {
+                    l.disqualification(DQ::State(None));
+                    continue;
+                }
+            }
+
+            if dry_run {
+                l.merge(
+                    "PR matches all criteria, I would've merged it if this weren't a dry run ✅",
+                    dry_run,
+                );
+            } else {
+                match client
+                    .pulls(&repo.owner, &repo.repo)
+                    .merge(pr.number)
+                    .method(config.merge_type.merge_method())
+                    .send()
+                    .await
+                    .context("couldn't merge PR")
+                {
+                    Ok(_) => {
+                        l.merge("PR merged! 🎉 ✅", dry_run);
+                    }
+                    Err(err) => {
+                        l.error(err);
+                    }
+                };
+            }
+        }
+    }
+
+    let end_ts = Utc::now();
+    let num_seconds = (end_ts - start).num_seconds();
+
+    l.empty_line();
+    l.info(&format!(
+        "This run ended at {}; took {} seconds",
+        end_ts, num_seconds
+    ));
+
+    l.write_output(output, output_file)
+        .context("couldn't write output to file")?;
+
+    Ok(())
+}
